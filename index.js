78834466014ec2604f06c0e9ca128c8018636112
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-15 15:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,78 +1,68 @@
-var cart = []
+var cart = [];
 
 function getCart() {
-  return cart
+ return cart;
 }
 
 function setCart(c) {
-  cart = c
+  cart = c;
+  return cart;
 }
 
 function addToCart(item) {
-  const price = Math.floor(Math.random() * 100)
+  var itemObj = {itemName:`${item}`, itemPrice: Math.floor(Math.random()*100)}
+  cart.push(itemObj);
+  return itemObj.itemName + " has been added to your cart."
 
-  cart.push({ [item]: price })
-
-  console.log(`${item} has been added to your cart.`)
-
-  return cart
 }
+
 
 function viewCart() {
-  const l = cart.length
-
-  if (!l) {
-    return console.log("Your shopping cart is empty.")
-  }
-
-  const itemsAndPrices = []
-
-  for (let i = 0; i < l; i++) {
-    let itemAndPrice = cart[i]
-    let item = Object.keys(itemAndPrice)[0]
-    let price = itemAndPrice[item]
-
-    itemsAndPrices.push(`${item} at \$${price}`)
-  }
-
-  console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`)
-}
-
-function removeFromCart(item) {
-  let itemInCart = false
-
-  for (let i = 0, l = cart.length; i < l; i++) {
-    if (cart[i].hasOwnProperty(item)) {
-      itemInCart = true
-      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    var myString = "In your cart, you have "
+    for(var i = 0; i < cart.length - 1; i++) {
+      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
   }
-
-  if (!itemInCart) {
-    console.log("That item is not in your cart.")
-  }
-
-  return cart
-}
-
-function placeOrder(cardNumber) {
-  if (!cardNumber) {
-    return console.log("We don't have a credit card on file for you to place your order.")
-  }
-
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-
-  cart = []
 }
 
 function total() {
-  let t = 0
+  total = 0;
+  for(var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
+}
 
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
+function removeFromCart(item) {
+
+
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1); 
+      return cart;
     }
   }
 
-  return t
+  if (item != cart[item]) {
+    return "That item is not in your cart."
+  }
+
+}
+
+function placeOrder(cardNumber) {
+
+  if (typeof(cardNumber) === "undefined"){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
 }
